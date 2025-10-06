591109d91e91661a2dfe56ff39c8dbdd37ce9b3d
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-06 17:02:40 +0200

    Aggiungi prodotti da API

---
+++ src/components/Product.jsx
@@ -0,0 +1,18 @@
+const Product = (props) => {
+
+    const { image, title, price, category } = props;
+
+    return (
+        <>
+            <div className="image">
+                <img src={image} alt="" />
+            </div>
+            <h2>{title}</h2>
+            <strong>{price}</strong>
+            <span>CATEGORY: {category}</span>
+        </>
+
+    )
+}
+
+export default Product
--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from 'react'
+import axios from "axios"
+import Product from '../components/product';
+
 const Prodotti = () => {
 
+    const [products, setProducts] = useState([]);
+
+    function fetchProducts() {
+        axios.get("https://fakestoreapi.com/products")
+            .then((res) =>
+                setProducts(res.data))
+            .catch(error => console.log(error)
+            )
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     return (
-        <p>Questa è la pagina Pordotti</p>
+        <div className="container">
+            {products.map(product =>
+                <div key={product.id} className="card">
+                    <Product
+                        image={product.image}
+                        title={product.title}
+                        price={product.price}
+                        category={product.category}
+                    />
+                </div>
+            )}
+        </div>
     )
 }
 
-export default Prodotti
+export default Prodotti 
