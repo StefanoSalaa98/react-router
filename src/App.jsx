7a49d116b9f9a131fa376f2f93492763a455e8ff
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-06 15:40:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import del layout
+import MyLayout from './layouts/MyLayout';
+
+import Homepage from './pages/Homepage';
+import ChiSiamo from './pages/ChiSiamo';
+import Prodotti from './pages/Prodotti';
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
