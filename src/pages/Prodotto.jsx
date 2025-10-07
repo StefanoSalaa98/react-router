5a5307105a7ba388c3696b56dad3de7e13ba3078
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-07 17:12:16 +0200

    Bonus: Aggiungi una navigazione programmatica che riporti alla pagina di listato se viene cercato un prodotto che non esiste

--- src/pages/Prodotto.jsx
+++ src/pages/Prodotto.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 // importo l'hook per poter leggere il parametro
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios"
-import { Link } from "react-router-dom";
 
 
 const Prodotto = () => {
@@ -10,14 +9,23 @@ const Prodotto = () => {
     // recupero il parametro dinamico id
     const { id } = useParams();
 
+    const navigate = useNavigate();
+
     // stato del componente (prodotto che è stato cliccato)
     const [product, setProduct] = useState();
 
     useEffect(() => {
         axios.get(`https://fakestoreapi.com/products/${id}`)
-            .then(res => setProduct(res.data))
+            .then(res => {
+                setProduct(res.data);
+                // se la API non restituisce niente perche l'id non è corretto, allora ricarico la pagina con la lista dei prodotti
+                if (res.data === "") {
+                    navigate("/products")
+                };
+            })
             .catch(err => console.log(err)
             )
+
     }, [])
 
     return (
