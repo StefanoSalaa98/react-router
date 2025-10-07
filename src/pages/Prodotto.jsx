c40c02f390a82378e6a224085760b8de1277617c
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-07 15:51:34 +0200

    Aggiungi letteura id tramite hook useParams

--- src/pages/Prodotto.jsx
+++ src/pages/Prodotto.jsx
@@ -1,7 +1,16 @@
+// importo l'hook per poter leggere il parametro
+import { useParams } from "react-router-dom";
+
 const Prodotto = () => {
 
+    // recupero il parametro dinamico id
+    const { id } = useParams();
+
     return (
-        <h2>Sezione dettaglio Prodotto</h2>
+        <>
+            <h2>Sezione dettaglio Prodotto</h2>
+            <h3>ID prodotto: {id}</h3>
+        </>
     )
 }
 
